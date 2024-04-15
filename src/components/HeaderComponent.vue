28ad70539d3a91f98ff52f97e863766f9f6f672c
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-15 22:20:45 +0200

    create header e main component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <nav class="navbar bg-light d-flex">
+                <img src="/images/logo.webp" alt="Logo">
+                <h1 class="mb-0 display-6">Yu-Gi-Oh Api</h1>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+img{
+    width: 120px;
+    padding: 30px;
+}
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+main{
+    height: calc(100vh - 50px);
+}
+</style>
